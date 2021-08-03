b3e0d893c76e2ff6d5ff720f161161d0eec6308d
J1910 <56464720+J1910@users.noreply.github.com>
2021-08-03 16:18:24 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -4,6 +4,7 @@ from flask import request
 def execute_user_command_unsafe(): 
     user_command = request.args.get('command')
     output = os.system(str(user_command))
+    output2 = os.system(str(user_command))
 	
 	
     return output
