d7747dd714e3c6be62a5f90bd229e2bbfd6a87a4
J1910 <56464720+J1910@users.noreply.github.com>
2022-08-09 10:15:00 +0900

    del
    del

--- py/Command_Injection.py
+++
@@ -1,8 +0,0 @@
-import os
-from flask import request
-
-def execute_user_command_unsafe(): 
-    user_command = request.args.get('command')
-    output = os.system(str(user_command))
-    return output
-	
