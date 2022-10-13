ec1e8e229527f1c31b71bbb5690dc9bcfcc67738
J1910 <56464720+J1910@users.noreply.github.com>
2022-10-13 10:30:51 +0900

    Add files via upload

---
+++ 日本語テスト/Command_Injection.py
@@ -0,0 +1,10 @@
+#//文字コードはShift-JISです。
+
+import os
+from flask import request
+
+def execute_user_command_unsafe(): 
+    user_command = request.args.get('command')
+    output = os.system(str(user_command))
+    return output
+	
