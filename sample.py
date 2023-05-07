214a95afe7a6cee9ddf46a8b550eccc240a1434d
J1910 <56464720+J1910@users.noreply.github.com>
2023-05-07 12:54:28 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -5,7 +5,7 @@ def execute_user_command_unsafe():
     user_command = request.args.get('command')
     output = os.system(str(user_command))
     output2 = os.system(str(user_command))
-	
+    output3 = os.system(str(user_command))
 	
     return output
 	
